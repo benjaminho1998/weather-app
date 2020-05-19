69516409bd50b40e35aa6c90d927adee6c44794c
Benjamin Ho <ho.d.benjamin@gmail.com>
2020-05-19 00:20:02 -0700

    Added weather API call

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './WeatherDisplay.css';
+import axios from 'axios';
 
 class WeatherDisplay extends React.Component {
 
     componentDidMount() {
-        //Call OpenWeather API here
+        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${this.props.lat}&lon=${this.props.lng}&units=imperial&exclude=minutely,daily&appid=b8cedfac1134c2a5008ccbe84ebddbea`)
+        .then(res => {
+            console.log(res);
+            //figure out how to convert unix to local time
+        })
     }
 
     render() {
